caa36fd9d99a525aeefbc65768e8f8c5aa523123
Anthony <zamora.anthony1234@gmail.com>
2020-08-03 21:46:05 -0400

    Add files via upload
    Blockchain can now import external files

--- CustomObject.py
+++ CustomObject.py
@@ -1,13 +1,16 @@
 import struct
 import os
 import subprocess
+import zipfile
 
 class CustomObject:
     
-    # OBJECTIVE: Created an empty class that represents a struct object similar in C++
+    # OBJECTIVE: Import files from host computer and put it in the blockchain
     
     def __init__(self):
-        pass
+        
+        # Hold file's path
+        self.file_path = os.environ['PWD']
 
     def string_to_struct(self, new_string):
         # OBJECTIVE: Convert string to bytes
@@ -46,13 +49,50 @@ class CustomObject:
         # Convert string to bytes
         return struct.pack('f', new_float)
 
-    def import_textfile(self):
-        # OBJECTIVE: To add a text file to the blockchain
+    def import_text_file(self):
+        # OBJECTIVE: Locate text file before importing to Python program
         
-        # Execute shell script and accept script's output as input
-        # FYI: subprocess will return data as bytes
-        output = subprocess.run(["bash", "FindFile.sh"], stdout=subprocess.PIPE)
+        # Ask for directory
+        file_directory = input("Enter file's directory: ")
 
-        # Return output as decoded string
-        return output.stdout.decode('UTF-8')
-        
+        # Search and replace text
+        # os.environ lets me to use shell variables on the host computer
+        if file_directory == "~":
+            file_directory = os.environ['HOME']
+        elif file_directory == ".":
+            file_directory = os.environ['PWD']
+
+        # Only print files inside directory
+        for pos, f in enumerate(os.listdir(file_directory)):
+            
+            if not os.path.isdir(file_directory + "/" + f):
+                print("{} - {}".format(pos, f))
+
+        # Ask for filename
+        file_name = input("Enter filename: ")
+
+        # Check if file exists
+        if not os.path.exists(file_directory + "/" + file_name):
+
+            print("Invalid directory!")
+            return None
+
+        # Save file's path
+        self.file_path = file_directory + "/" + file_name
+
+        print("Compression is starting")
+
+        # Compress file
+        zf = zipfile.ZipFile("{}.zip".format(file_name), mode='w') # <- Create zip's filename
+        zf.write(self.file_path, compress_type=zipfile.ZIP_STORED) # <- Enter file to compress
+        zf.close() # <- Close the zip file
+
+        print("Compression is done")
+
+        # Open file in read binary mode
+        with open("{}.zip".format(file_name), "rb") as f:
+
+            f_message = f.readlines() # <- Read message and return list
+            f_message = b' '.join(word for word in f_message) # <- Turn list to string
+            
+            return f_message
