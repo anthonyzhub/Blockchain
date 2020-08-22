531822f7302d6602d6a595cb9aad032a1d5b0d84
Anthony <zamora.anthony1234@gmail.com>
2020-08-22 14:22:24 -0400

    Add files via upload
    Made some convention changes. Users can download the entire blockchain. Asymmetric encryption is the replacement of BLAKE2.

--- Link.py
+++ Link.py
@@ -1,5 +1,6 @@
 from Block import Block
 from Hash import Hash
+from Signature import Signature
 
 import numpy as np
 import pandas as pd
@@ -26,23 +27,27 @@ class LinkList:
         # Set a data frame
         self.data_frame = pd.DataFrame()
 
+        # Initialize Signature class
+        self.sig = Signature()
+
     def get_genesis_block(self):
         # OBJECTIVE: Get head node
+
         return self.genesis_block
 
     def get_size(self):
         # OBJECTIVE: Get list's size
+
         return self.list_size
 
     def is_empty(self):
         # OBJECTIVE: Check if list is empty
+
         return self.get_size() == 0
 
+    """
     def add_block(self, new_unencrypted_data):
         # OBJECTIVE: Append a block at the end of the chain
-
-        # Create instance of Hash class
-        self.hash_class = Hash()
 
         # Encrypt data
         new_encrypted_data = self.hash_class.encrypt_data(new_unencrypted_data)
@@ -60,6 +65,60 @@ class LinkList:
             # Add encrypted and unencrypted data to block
             new_head_block.previous_block_hash = None
             new_head_block.current_block_hash = new_encrypted_data
+            new_head_block.current_block_data = new_unencrypted_data # Add unencrypted data for verification later on
+
+            # Set new_head_block as genesis_block
+            self.genesis_block = new_head_block
+
+        else:
+
+            # Create a new node and pass incoming_hash to it
+            new_block = Block()
+            new_block.current_block_hash = new_encrypted_data
+            new_block.current_block_data = new_unencrypted_data
+
+            # Go to last block inside of chain
+            current_block = self.genesis_block
+
+            while current_block.next_block_pointer is not None:
+                current_block = current_block.next_block_pointer
+
+            # Update current_block's pointer and hash value
+            current_block.next_block_pointer = new_block
+
+            # Update new_block's pointers and hash values
+            new_block.previous_block_hash = self.previous_hash_static
+            new_block.prev_block_pointer = current_block
+            new_block.next_block_pointer = None
+
+        # Update list's length
+        self.list_size += 1
+
+        # Save new encrypted data to previous_hash_static
+        self.previous_hash_static = new_encrypted_data
+    """
+
+    def add_block(self, new_unencrypted_data):
+        # OBJECTIVE: Append a block at the end of the chain
+
+        # Encrypt data
+        new_encrypted_data = self.sig.agenda(new_unencrypted_data)
+
+        # If list is empty, automatically add incoming_hash as head block
+        if self.is_empty():
+
+            # Create a new block
+            new_head_block = Block()
+
+            # Create links for block
+            new_head_block.prev_block_pointer = None
+            new_head_block.next_block_pointer = None
+            
+            # Add hash value from previous' and current block
+            new_head_block.previous_block_hash = None
+            new_head_block.current_block_hash = new_encrypted_data
+
+            # Add plain data to block
             new_head_block.current_block_data = new_unencrypted_data # Add unencrypted data for verification later on
 
             # Set new_head_block as genesis_block
--- Signature.py
+++ Signature.py
@@ -1,6 +1,7 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
+import cryptography.exceptions
 import os
 
 # Documentation: https://cryptography.io/en/latest/hazmat/primitives/asymmetric/rsa/
@@ -13,68 +14,85 @@ class Signature:
         
         self.private_key = None
         self.public_key = None
-        self.user_name = self.user_name = os.environ['USER'] # <- Cannot find 'WHOAMI'
-        self.ENC_ALGO_TYPE = hashes.SHA256()
+        self.signature = None
+        self.user_name = os.environ['USER'] # <- Cannot find 'WHOAMI'
+        # hashes.SHA256() = hashes.SHA256()
         self.PRIVATE_FILE_NAME = "{}-Private_Key.pem".format(self.user_name)
         self.PUBLIC_FILE_NAME = "{}-Public_Key.pem".format(self.user_name)
+        # "UTF-8" = "UTF-8"
+
+    def format_data(self, data):
+
+        # OBJECTIVE: Change data to string as bytes and change it to a string if needed
+
+        if type(data) is str:
+
+            # Transform data to bytes
+            data = bytes(data, "UTF-8")
+
+        else:
+
+            # Change data type and transform it
+            data = bytes(str(data), "UTF-8")
+
+        # Return edited data
+        return data
 
     def generate_keys(self):
 
         # OBJECTIVE: To generate a public-private key using RSA
 
         """
+        NOTE:
         1. public_exponent - The public exponent of the new key. Default is 65537.
         2. key_size - How big the key is in bits.
         3. backend - Generates a new RSA private from default_backend(). This implements RSABackend.
         """
+
         # Generate a pair of keys
         self.private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
 
         # From the private key derived the public key
         self.public_key = self.private_key.public_key()
 
-    def get_keys(self):
+        print("=== Generated Keys ===")
+
+    def load_keys(self):
 
         # OBJECTIVE: Collect user's keys if they were already created
 
         # NOTE: If keys were created, check project's working directory
 
-        # Create boolean variable for further use
-        does_private_key_exist = False
-        does_public_key_exist = False
-        
-        # Open private key PEM file
-        with open(self.PRIVATE_FILE_NAME, "rb") as private_file:
+        try:
 
-            # Save private_file's content to private_key
-            self.private_key = serialization.load_pem_private_key(
-                private_file.read(),
-                password=None, # <- Password wasn't given upon creation
-                backend=default_backend()
-            )
+            # Open private key PEM file
+            with open(self.PRIVATE_FILE_NAME, "rb") as private_file:
 
-            # Set boolean to True
-            does_private_key_exist = True
+                # Save private_file's content to private_key
+                self.private_key = serialization.load_pem_private_key(
+                    private_file.read(),
+                    password=None, # <- Password wasn't given upon creation
+                    backend=default_backend()
+                )
 
-        # Open public key PEM file
-        with open(self.PUBLIC_FILE_NAME, "rb") as public_file:
+            # Open public key PEM file
+            with open(self.PUBLIC_FILE_NAME, "rb") as public_file:
 
-            # Save public_file's content to public_key
-            self.public_key = serialization.load_pem_public_key(
-                public_file.read(),
-                backend=default_backend()
-            )
+                # Save public_file's content to public_key
+                self.public_key = serialization.load_pem_public_key(
+                    public_file.read(),
+                    backend=default_backend()
+                )
 
-            # Set boolean to True
-            does_public_key_exist = True
-
-        # Return true if both conditions are true
-        if does_private_key_exist == True and does_public_key_exist == True:
+            print("=== Loaded Keys ===")
 
             return True
 
-        else:
+        except FileNotFoundError as err:
             
+            print("Couldn't find or open file(s), so new keys will be generated!")
+            print("Error Message: {}\n".format(err))
+
             return False
 
     def write_keys(self):
@@ -82,9 +100,13 @@ class Signature:
         # OBJECTIVE: To write the keys in a PEM file
 
         # Create a PEM file for the private key. The command below will serialize it.
-        private_pem_file = self.private_key.private_bytes(encoding=serialization.Encoding.PEM, format=serialization.PrivateFormat.PKCS8, encryption_algorithm=serialization.NoEncryption())
+        private_pem_file = self.private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
 
-        # Write the file in binary format to the file
+        # Write the file in binary format
         with open(self.PRIVATE_FILE_NAME, "wb") as private_file:
 
             # Write private key to file
@@ -93,83 +115,85 @@ class Signature:
         # Create a file for the public key. The command below will serialize it
         public_pem_file = self.public_key.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo)
         
-        # Write the file in binary format to the file
+        # Write the file in binary format
         with open(self.PUBLIC_FILE_NAME, "wb") as public_file:
 
             # Write public key to file
             public_file.write(public_pem_file)
 
-    def create_signature(self, block_data):
+        print("=== Wrote Keys ===")
+
+    def sign_block_data(self, block_data):
 
         # OBJECTIVE: To sign binary data with user's private key
 
         """
+        NOTE:
         A signature requires a specific hash function and padding to use
         PSS is recommended for protocols or applications. 
-        PKCS1v15 supports legacy protocols
+        PKCS1v15 supports legacy protocols.
+
+        Also, signing and verifying must be done within the same function, or signature will be invalid.
         """
 
         # Digitally sign a block's data
-        signature = self.private_key.sign(
+        self.signature = self.private_key.sign(
             block_data,
             padding.PSS(
-                mgf=padding.MGF1(self.ENC_ALGO_TYPE),
+                mgf=padding.MGF1(hashes.SHA256()),
                 salt_length=padding.PSS.MAX_LENGTH
                 ),
-            self.ENC_ALGO_TYPE
+            hashes.SHA256()
         )
 
-        # Return signature
-        return signature
+        print("=== Signed Data ===")
 
-    def verify_signature(self, signature, block_data):
-
-        # OBJECTIVE: To verify user's signature on their uploaded data
-
-        is_signature_valid = False
-
-        # Attempt to verify signature on block's data
+        # Verify signature by decrypting signed data with signature
         try:
-
-            # Verify signature by decrypting signed data with signature
+            
             self.public_key.verify(
-                signature,
+                self.signature,
                 block_data,
                 padding.PSS(
-                    mgf=padding.MGF1(self.ENC_ALGO_TYPE),
+                    mgf=padding.MGF1(hashes.SHA256()),
                     salt_length=padding.PSS.MAX_LENGTH
                 ),
-                self.ENC_ALGO_TYPE
+                hashes.SHA256()
             )
 
-            # Change verification status
-            is_signature_valid = True
+            print("=== Signature Verified ===")
 
-        except:
+        except cryptography.exceptions.InvalidSignature as err:
 
-            print("*** Invalid signature! ***")
-
-        # Return status
-        return is_signature_valid
+            # Print error message and exit program
+            print("Error message: {}".format(err))
+            exit()
 
     def encrypt_data(self, unencrypted_data):
 
         # OBJECTIVE: To encrypt data before adding it to blockchain
 
         """
+        NOTE:
         Valid paddings for encryption are OAEP and PKCS1v15.
         OAEP is recommended for protocols or applications.
         PKCS1v15 should be used to support legacy protocols.
         """
+
+        # Change unencrypted_data to bytes
+        unencrypted_data = self.format_data(unencrypted_data)
+
         # Encrypt data with user's public key using SHA256
         cipher = self.public_key.encrypt(
             unencrypted_data,
             padding.OAEP(
-                mgf=padding.MGF1(algorithm=self.ENC_ALGO_TYPE),
-                algorithm=self.ENC_ALGO_TYPE,
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
                 label=None
             )
         )
+
+        print("=== Encrypted Data ===")
 
         # Return cipher text
         return cipher
@@ -182,11 +206,33 @@ class Signature:
         plain = self.private_key.decrypt(
             cipher,
             padding.OAEP(
-                mgf=padding.MGF1(algorithm=self.ENC_ALGO_TYPE),
-                algorithm=self.ENC_ALGO_TYPE,
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
                 label=None
             )
         )
 
+        print("=== Decrypted Data ===")
+
         # Return decrypted data
         return plain
+
+    def agenda(self, data):
+
+        # OBJECTIVE: Use this function to call other functions
+
+        # Check if keys PEM file already exist
+        if self.load_keys() == False:
+
+            # Generate and save keys
+            self.generate_keys()
+            self.write_keys()
+
+        # Encrypt data
+        cipher = self.encrypt_data(data)
+
+        # Sign encrypted data and verify signature
+        self.sign_block_data(cipher)
+
+        # Return encrypted data
+        return cipher
