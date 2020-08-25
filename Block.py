42adf487a36ca6a22f3a6bd71e618816c6b97886
Anthony Zamora <zamora.anthony1234@gmail.com>
2020-08-24 20:57:03 -0400

    Changed/removed functions

--- Block.py
+++ Block.py
@@ -1,4 +1,3 @@
-import time
 import json
 
 from hashlib import sha256
@@ -7,8 +6,7 @@ class Block:
 
     # OBJECTIVE: To create components needed to make a block
 
-    # Class initializer
-    def __init__(self, index, data, timestamp, previous_block_hash, current_block_hash, nonce=0):
+    def __init__(self, index, data, timestamp, previous_block_hash, nonce=0):
         
         # Block's position and plain data
         self.index = index
@@ -18,17 +16,24 @@ class Block:
         self.timestamp = timestamp
 
         # Save hashes
-        self.previous_block_hash = previous_block_hash
-        self.current_block_hash = current_block_hash
+        self.previous_block_hash = previous_block_hash # <= Copied from previous block
+        self.current_block_hash = None # <= Yet to be computed
+
+        # Create pointers for network
+        self.previous_block = None
+        self.next_block = None
 
         self.nonce = nonce
 
     def compute_hash(self):
 
         # OBJECTIVE: Compute hash value of block's contents
+        # NOTE: Function will return the same hash value, if given the same input. It doesn't matter how many times!
         
         # Create a dictionary in JSON format and sort keys
         json_string = json.dumps(self.__dict__, sort_keys=True)
+
+        # print(json_string)
 
         # Return hash value of block's content
         return sha256(json_string.encode()).hexdigest()
--- Link.py
+++ Link.py
@@ -1,160 +1,212 @@
 from Block import Block
-# from Hash import Hash
-from Signature import Signature
+from collections import deque
 
 import numpy as np
 import pandas as pd
 import zipfile
 import os
+import time
 
-class LinkList:
+class Blockchain:
 
     # Declare static variable for add_block()
     # A static variable's value will always be remembered until program is terminated
     previous_hash_static = None
 
+    # Difficulty of PoW algorithm
+    difficulty = 2
+
     def __init__(self):
         
-        # Set head node as blank
-        self.genesis_block = None
+        # Create a head and tail node
+        self.head = None
+        self.tail = None
 
-        # Set starting size to 0
+        # Set linked list size to 0
         self.list_size = 0
 
-        # Set a data frame
+        # Create a queue of unconfirmed transactions
+        self.unconfirmed_transactions = deque()
+
+        # Create a data frame
         self.data_frame = pd.DataFrame()
 
-        # Initialize Signature class
-        self.sig = Signature()
-
-    def get_genesis_block(self):
-        # OBJECTIVE: Get head node
-
-        return self.genesis_block
-
-    def get_size(self):
-        # OBJECTIVE: Get list's size
-
-        return self.list_size
-
     def is_empty(self):
+
         # OBJECTIVE: Check if list is empty
+        return self.list_size == 0
 
-        return self.get_size() == 0
+    def create_hash(self, block):
 
-    def add_block(self, new_unencrypted_data):
-        # OBJECTIVE: Append a block at the end of the chain
+        # OBJECTIVE: Before officially adding a block, increment block's nonce value until a proper hash value is computed
 
-        # Encrypt data
-        new_encrypted_data, public_key = self.sig.agenda(new_unencrypted_data)
+        """
+        NOTE: A nonce is created to see how many times it will take for the computer to create a valid hash.
+                A hash is valid, if it has a leading number of 0s. In this case, the number of zeros depends
+                on the level of difficulty. Hence, the variable "difficulty" that is declared at the beginning
+                of this class. 
 
-        # If list is empty, automatically add incoming_hash as head block
+                This approach is brute force because we are starting at 0. The variable will increment until
+                a proper hash is calculated. So, nonce could eventually be 1_000_000_000!
+        """
+
+        # Start nonce value as 0
+        block.nonce = 0
+
+        # Compute block's hash
+        hash = block.compute_hash()
+
+        # Continue to compute a new hash until it starts with a N number of 0s.
+        # Ex. 00xxxxx
+        while not hash.startswith("0" * self.difficulty):
+
+            # Increment block's nonce value and compute new hash
+            block.nonce += 1
+            hash = block.compute_hash()
+
+        return hash
+
+    def is_hash_valid(self, new_block, proposed_hash):
+
+        # OBJECTIVE: Check if hash value meets the requirements AND hash value matches with a newly created hash value
+        #
+        #   1. Does proposed_hash start with N number of 0s?
+        #   2. If hash is recomputed, would it return the same string?
+        return (proposed_hash.startswith("0" * self.difficulty) and proposed_hash == new_block.compute_hash())
+
+    def validate_entry(self, new_block, proposed_hash):
+
+        # OBJECTIVE: Verify block can be added to link list by:
+        #
+        #   1. Does new_block have the correct previous_block_hash?
+        #   2. Does new_block's hash meet the requirements
+
+        # Get hash from last block
+        old_hash = self.tail.current_block_hash
+
+        # Stop if hash values don't match
+        if new_block.previous_block_hash != old_hash:
+            return False
+
+        # Stop if block's hash value isn't valid
+        if not self.is_hash_valid(new_block, proposed_hash):
+            return False
+
+        # If hash value is valid, then add it to the new block
+        new_block.current_block_hash = proposed_hash
+
+        return True
+
+    def create_head(self, data):
+
+        # OBJECTIVE: Create a head block with previous_hash as 0
+
+        print("Creating a head block")
+
+        # Create a head block with a hash
+        head_block = Block(self.list_size, data, time.time(), "0")
+        head_block.current_block_hash = self.create_hash(head_block)
+
+        # Add it to link list
+        self.head = head_block
+        self.tail = head_block
+
+        # Update previous_hash_static variable
+        self.previous_hash_static = head_block.current_block_hash
+
+        # Increment counter for list size
+        self.list_size += 1
+
+    def add_block(self, data):
+
+        # OBJECTIVE: Add a block to link list
+
+        # If list is empty, go to a dedicated function to create a head block
         if self.is_empty():
 
-            # Create a new block
-            new_head_block = Block()
+            self.create_head(data)
 
-            # Create links for block
-            new_head_block.prev_block_pointer = None
-            new_head_block.next_block_pointer = None
-            
-            # Add hash value from previous' and current block
-            new_head_block.previous_block_hash = None
-            new_head_block.current_block_hash = new_encrypted_data
+        print("Creating a new block")
 
-            # Add plain data to block
-            new_head_block.current_block_data = new_unencrypted_data # Add unencrypted data for verification later on
+        # Create a new block and compute a hash value
+        new_block = Block(self.list_size, data, time.time(), self.previous_hash_static)
+        new_block.current_block_hash = self.create_hash(new_block)
 
-            # Add user's public key
-            new_head_block.uploader_public_key = public_key
+        # Get last block to update pointers
+        old_block = self.tail
 
-            # Set new_head_block as genesis_block
-            self.genesis_block = new_head_block
+        old_block.next_block = new_block
 
-        else:
+        new_block.previous_block = old_block
+        new_block.next_block = None
 
-            # Create a new block
-            new_block = Block()
+        # Update tail
+        self.tail = new_block
 
-            # Add hash, unencrypted data, and public key to the new block
-            new_block.current_block_hash = new_encrypted_data
-            new_block.current_block_data = new_unencrypted_data
-            new_block.uploader_public_key = public_key
-
-            # Go to last block inside of the chain
-            current_block = self.genesis_block
-
-            while current_block.next_block_pointer is not None:
-                current_block = current_block.next_block_pointer
-
-            # Update current_block's pointer
-            current_block.next_block_pointer = new_block
-
-            # Update new_block's pointers and hash values
-            new_block.previous_block_hash = self.previous_hash_static
-            new_block.prev_block_pointer = current_block
-            new_block.next_block_pointer = None
+        # Update old hash value
+        self.previous_hash_static = new_block.current_block_hash
 
         # Update list's length
         self.list_size += 1
 
-        # Save new encrypted data to previous_hash_static
-        self.previous_hash_static = new_encrypted_data
-
     def verify_chain(self):
 
-        # OBJECTIVE: Verify block chain was not tampered with by comparing hashes between 2 blocks
-
-        # Check if list is empty or only has 1 block inside the chain
+        # Exit if link list only has <=1 block
         if self.is_empty() or self.list_size == 1:
-            
-            print("Blockchain is empty or only has 1 block.")
-            return None
+            print("There's only <=1 blocks in the link list")
+            return True
 
-        # Get 1st 2 block
-        current_block = self.genesis_block
-        next_block = current_block.next_block_pointer
+        # Get starting blocks
+        tmp_block_a = self.head
+        tmp_block_b = tmp_block_a.next_block
 
-        while next_block is not None:
-            
-            # Return "False" if hashes between current_block and next_block don't match
-            if current_block.current_block_hash != next_block.previous_block_hash:
-                
-                print("Invalid chain!")
+        counter = 0
+
+        # Iterate link list
+        while tmp_block_b is not None:
+
+            # Compare hash values
+            if tmp_block_b.previous_block_hash != tmp_block_a.current_block_hash:
+                print("ERROR: Block #{} has a different hash value than Block #{}!".format(counter, counter - 1))
                 return False
 
-            # Move to next block
-            current_block = next_block
-            next_block = next_block.next_block_pointer
+            # Update temporary blocks
+            tmp_block_a = tmp_block_b
+            tmp_block_b = tmp_block_b.next_block
 
-        # Return "True" if previous if-statement wasn't triggered
+            # Increment counter
+            counter += 1
+
         return True
+
 
     def print_list(self):
 
-        # OBJECTIVE: Print block chain
+        # OBJECTIVE: Print blockchain
 
+        # Exit if link list is empty
         if self.is_empty():
             
             print("List is empty!")
             return None
 
-        # Get starting block
-        current_block = self.genesis_block
+        # Get head block
+        old_block = self.head
+        counter = 0
 
-        while current_block is not None:
+        while old_block is not None:
 
             # Print block's contents
             print("\nBlock Data:")
-            print("\tHash: {}".format(current_block.current_block_hash))
-            print("\tData: {}".format(current_block.current_block_data))
-            print("\tUploader's public key: {}".format(current_block.uploader_public_key))
+            print("\tHash: {}".format(old_block.current_block_hash))
+            print("\tData: {}".format(old_block.data))
+            counter += 1
 
             # Move to next block
-            current_block = current_block.next_block_pointer
+            old_block = old_block.next_block
 
         # Print total number of blocks inside chain
-        print("\nTotal Blocks: {}".format(self.list_size))
+        print("\nTotal Blocks: {}".format(counter))
 
     def download_blockchain_data(self):
 
@@ -162,20 +214,22 @@ class LinkList:
 
         # Check if list is empty
         if self.is_empty() is True:
+
+            print("List is empty!")
             return None
 
         # Get genesis block
-        current_block = self.genesis_block
+        old_block = self.head
         dict_block = dict()
         block_num = 0
 
-        while current_block is not None:
+        while old_block is not None:
 
             # Write block's data to dictionary
-            dict_block["Previous Block's Hash"] = current_block.previous_block_hash
-            dict_block["Current Block's Hash"] = current_block.current_block_hash
+            dict_block["Previous Block's Hash"] = old_block.previous_block_hash
+            dict_block["Current Block's Hash"] = old_block.current_block_hash
 
-            dict_block["Current Block's Data"] = current_block.current_block_data
+            dict_block["Current Block's Data"] = old_block.current_block_data
 
             # Save dictionary to data frame
             self.data_frame = self.data_frame.append(dict_block, ignore_index=True)
@@ -190,7 +244,7 @@ class LinkList:
             block_num+=1
 
             # Move to next block
-            current_block = current_block.next_block_pointer
+            old_block = old_block.next_block_pointer
 
         # Write dataframe to CSV file
         csv_file_name = "Zamora-Blockchain_Data.csv"
